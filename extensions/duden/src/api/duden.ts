8b7765f56416860d1142d340a33b39abdd2dc1c9
Anselm Hinderling <aha@absinth.net>
2026-04-15 16:21:53 +0200

    change user-agent string to not impersonate a chrome browser

--- extensions/duden/src/api/duden.ts
+++ extensions/duden/src/api/duden.ts
@@ -60,8 +60,7 @@ async function makeRequest(url: string): Promise<string> {
     const response = await fetch(url, {
       signal: controller.signal,
       headers: {
-        "User-Agent":
-          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        "User-Agent": "Raycast Duden Extension (https://github.com/raycast/extensions)",
       },
     });
 
